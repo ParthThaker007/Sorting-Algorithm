d2ce2d6dc9a2f440ebe2a338b7ad496652e6cc7f
ParthThaker007 <101121538+ParthThaker007@users.noreply.github.com>
2023-06-24 15:26:30 +0530

    Update Insertion_sort.cpp

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -1,3 +1,11 @@
+// Insertion sort is a simple sorting algorithm that works similar to the way you sort playing cards in your hands.
+// The array is virtually split into a sorted and an unsorted part.
+// Values from the unsorted part are picked and placed at the correct position in the sorted part.
+// Time Complexity of Insertion Sort is :
+// Best Case = O(n)   Average Case = O(n^2)    Worst Case = O(n^2)
+// Space Complexity = O(1)
+
+
 #include<iostream>
 using namespace std;
 void Insertion_sort(int arr[],int n){
